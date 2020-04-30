985531393347c9008e0c5076d325078680380bc3
Betsy <betsy.yoon@gmail.com>
2020-04-30 14:26:31 -0400

    moved font config files to main emacs file....don't understand why

--- custom.el
+++ custom.el
@@ -3,11 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fringe-mode 50 nil (fringe))
- '(ring-bell-function (quote ignore)))
+ '(org-modules
+   (quote
+    (ol-bibtex ol-eww org-habit org-id org-protocol ol-w3m))))
+ 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(bold ((t (:foreground "#f0810f" :weight bold))))
+ '(bold-italic ((t (:inherit italic)))))
