00620813608512317b7f0997c0e54399ed374387
Betsy <betsy.yoon@gmail.com>
2020-05-28 11:19:29 -0400

    daily update

--- custom.el
+++ custom.el
@@ -36,6 +36,7 @@
    (quote
     (ol-bibtex ol-eww org-habit org-id org-protocol ol-w3m org-collector org-notify)))
  '(org-pomodoro-keep-killed-pomodoro-time t)
+ '(org-pomodoro-ticking-sound-states (quote (:pomodoro)))
  '(org-tag-faces
    (quote
     (("ndd" :inherit org-tag :foreground "yellow green")
@@ -49,7 +50,11 @@
      ("create" :inherit org-tag :foreground "dark orange")
      ("admin" :inherit org-tag :foreground "dark orange")
      ("lis" :inherit org-tag :foreground "yellow green")
-     ("research" :inherit org-tag :foreground "dark orange"))))
+     ("research" :inherit org-tag :foreground "dark orange")
+     ("self" :inherit org-tag :foreground "yellow green")
+     ("journal" :inherit org-tag :foreground "dark orange")
+     ("cleaning" :inherit org-tag :foreground "dark orange")
+     ("job" :inherit org-tag :foreground "dark orange"))))
  '(org-todo-keyword-faces
    (quote
     (("WAIT" :weight regular :underline nil :inherit org-todo :foreground "yellow")
@@ -57,11 +62,12 @@
      ("TODO" :weight regular :underline nil :inherit org-todo :foreground "#e8a735")
      ("HABIT" :weight regular :underline nil :inherit org-todo :foreground "brown")
      ("AREA" :weight bold :underline t :inherit org-todo :foreground "dodger blue")
-     ("NEXT" :weight regular :underline nil :inherit org-todo :foreground "magenta"))))
+     ("NEXT" :weight regular :underline nil :inherit org-todo :foreground "magenta")
+     ("IN-PROG" :weight regular :underline nil :inherit org-todo :foreground "lightcyan1" :background "royalblue1"))))
  '(org-web-tools-pandoc-sleep-time 1.0)
  '(package-selected-packages
    (quote
-    (tomatinho pomidor org-pomodoro org-plus-contrib crux org-noter pdf-view-restore org-alert anki-editor anki-connect ace-jump-buffer ace-jump-mode helm-org-rifle eyebrowse org-download which-key ace-jump-helm-line ace-link expand-region yasnippet-snippets yankpad w3m visual-fill-column unicode-fonts ssh-agency spotify speed-type smartparens slack rainbow-delimiters powerthesaurus pandoc-mode pandoc ox-pandoc ox-clip ov org-web-tools org-sticky-header org-ref org-recent-headings org-preview-html org-pdfview org-edna org-cliplink org-chef org-bullets org-bookmark-heading openwith nov multiple-cursors mixed-pitch markdown-preview-eww markdown-mode magit-popup magit ledger-mode latex-preview-pane keychain-environment interleave html2org hl-anything highlight helm-swoop helm-spotify helm-projectile helm-chronos helm-bibtexkey gnuplot-mode gnuplot gnu-elpa-keyring-update flyspell-correct-helm ereader doom-themes diminish deft define-word counsel company-bibtex company-auctex centered-window centered-cursor-mode calfw-org calfw-cal calfw bufler all-the-icons aggressive-indent ace-window 2048-game)))
+    (org-clock-convenience tomatinho pomidor org-pomodoro org-plus-contrib crux org-noter pdf-view-restore org-alert anki-editor anki-connect ace-jump-buffer ace-jump-mode helm-org-rifle eyebrowse org-download which-key ace-jump-helm-line ace-link expand-region yasnippet-snippets yankpad w3m visual-fill-column unicode-fonts ssh-agency spotify speed-type smartparens slack rainbow-delimiters powerthesaurus pandoc-mode pandoc ox-pandoc ox-clip ov org-web-tools org-sticky-header org-ref org-recent-headings org-preview-html org-pdfview org-edna org-cliplink org-chef org-bullets org-bookmark-heading openwith nov multiple-cursors mixed-pitch markdown-preview-eww markdown-mode magit-popup magit ledger-mode latex-preview-pane keychain-environment interleave html2org hl-anything highlight helm-swoop helm-spotify helm-projectile helm-chronos helm-bibtexkey gnuplot-mode gnuplot gnu-elpa-keyring-update flyspell-correct-helm ereader doom-themes diminish deft define-word counsel company-bibtex company-auctex centered-window centered-cursor-mode calfw-org calfw-cal calfw bufler all-the-icons aggressive-indent ace-window 2048-game)))
  '(pdf-annot-default-annotation-properties
    (quote
     ((t
@@ -101,6 +107,7 @@
  '(org-link ((t (:inherit link :foreground "chocolate" :weight normal :family "monofur"))))
  '(org-special-keyword ((t (:foreground "#83898d" :height 0.9))))
  '(org-tag ((t (:foreground "#83898d" :weight normal :height 0.8))))
+ '(org-tag-group ((t (:inherit nil :foreground "dodger blue"))))
  '(org-todo ((t (:foreground "#98be65" :weight normal :family "nova mono"))))
  '(underline ((t (:foreground "#89da59" :underline t))))
  '(variable-pitch ((t (:height 1.0 :family "koho")))))
