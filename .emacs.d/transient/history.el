451ce984dd912df99c1c83b0f2b85d270516b54e
Betsy <betsy.yoon@gmail.com>
2023-08-18 10:03:15 -0400

    weekly update

--- .emacs.d/transient/history.el
+++ .emacs.d/transient/history.el
@@ -4,7 +4,7 @@
  (magit-gitignore nil)
  (magit-pull nil)
  (magit-push nil)
- (magit-remote\.<remote>\.*url "https://github.com/tiktaaliki/emacs.git" "git@github.com:tiktaaliki/emacs")
+ (magit-remote.<remote>.*url "https://github.com/tiktaaliki/emacs.git" "git@github.com:tiktaaliki/emacs")
  (magit-revert
   ("--edit"))
  (magit-tag nil)
